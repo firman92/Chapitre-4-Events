2e2ac2c741d0a1b1c02632d4925dd4e1af600b85
firman92 <firman92@live.fr>
2021-08-19 17:16:58 +0200

    finish exo 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "./react";
+import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -14,6 +14,68 @@ class App extends React.Component {
       passwordIsValid: false,
       isSubmitted: false,
     };
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({ email: e.target.value, emailIsValid: true });
+    console.log(this.state.email);
+    console.log(this.state.emailIsValid);
+  };
+  handlePasswordChange = (e) => {
+    this.setState({ email: e.target.value, emailIsValid: true });
+    console.log(this.state.email);
+    console.log(this.state.emailIsValid);
+  };
+
+  render() {
+    return (
+      <>
+        <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              onChange={this.handleEmailChange}
+              autoComplete={"off"}
+              onSubmit={this.handleEmailChange}
+            />
+            <div id="emailHelp" class="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              onChange={this.handleEmailChange}
+            />
+          </div>
+          <div class="mb-3 form-check">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="exampleCheck1"
+              onChange={this.handleEmailChange}
+            />
+            <label class="form-check-label" for="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </>
+    );
   }
 }
 
